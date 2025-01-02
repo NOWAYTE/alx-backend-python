522b2aa608934e18fa811ec675e4425b0217375b
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-02 05:27:44 +0300

    Complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Complex types annotations"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns sum of the list"""
+    return sum(input_list)
