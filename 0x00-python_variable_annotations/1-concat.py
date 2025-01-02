e1ccd54450720886d7f48f88a78e2b599b397497
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-02 04:51:58 +0300

    Code Update

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """String concat"""
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     """Returns concat string"""
     return "{}{}".format(str1, str2)
