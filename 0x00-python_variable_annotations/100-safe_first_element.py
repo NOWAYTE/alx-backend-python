8c88408d4cf1e7c6a5a4f5567af91a07a6f7c6da
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-12 17:35:08 +0300

    Update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Duck typing first element of a sequence"""
-from collections.abc import Sequence
-from typing import Union, TypeVar, Any
+"""Duck typing first letter of a sequence"""
 
+from typing import List, Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return first element"""
+
     if lst:
         return lst[0]
     else:
         return None
-
