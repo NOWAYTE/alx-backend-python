38e51a1775f81f6389a845b38881ee7b885d26d0
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-23 22:47:04 +0300

    Code update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
