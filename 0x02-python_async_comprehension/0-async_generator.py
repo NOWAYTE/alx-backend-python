e1a7937016b2d3d06befee6ca205cdf6689310b6
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-23 22:42:57 +0300

    Async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async generator"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yields a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
